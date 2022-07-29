c009453694c455b8c0d8c96f52d1759c51a71711
Barry Irwin <bvi@moria.org>
2022-07-29 21:38:04 +0200

    First refactoring to handle command line arguments

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
 # gennet
+
 Subnet worksheet generator
 
-This script was developed to aid in the need for drill and practice exercises for  teachingan introductory networks course.
+This script was developed to aid in the need for drill and practice exercises for teaching an introductory networks course. This has devleoped over the years to add additional functionality and use cases.
 
-Worksheets contents are randomly generated and outputted as a PDF document. Options allow for the generation of a two-sided worksheet containing solutions. Alternately this can be suppressed, for more summative type exercises
+The worksheets contents are randomly generated and outputted as (a minimal) LaTeX document. This document (or select parts of it) can then be converted to a PDF using pdflatex or similar. 
+
+Options allow for the generation of a two-sided worksheet containing solutions. Alternately this can be suppressed, for more summative type exercises.
+
+The problems and solutions tables can each be generated separately for inclusionas part of a larger LaTeX document.
+
+Seed values are stroed in the LaTeX versions of the output. These can be used to regenerate a particular set of output (for example a solution) at a later stage.
+
+##USAGE
+This produced a list of IPv4 subnets to stdout in a variety of formats
+
+
+ ./gennet.py [-hV] [-px] [-m <num>] [-s <num] [-L [-o val] | -C] 
+  -h     This usage guide
+  -m <n> Number of IP subnets to generate  (default=20)
+  -o <type> Output type for partial output in LaTex mode
+           soln - Solutions table
+           prob - Problems table
+  -p     Include RFC1918/RFC 3300 special use IP addresses (default=no)
+  -x     Do not print the example row in the problems table
+  -L     Latex Documnet output mode
+  -C     CSV output mode
+  -S <n> Seed (int or hex - [0x????] )
+  -V    Version and source information
+
+Minimum examples:
+	./gennet.py -L # prints a ful LaTeX dump
+	./gennet.py -C # prints CSV output
+
+Conversion:
+	$ ./gennet.py -L > temp.tex; pdflatex temp.tex
+
+$Id$
+
--- gennet.py
+++ gennet.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
 
-# gennet.py  v0.2 
+# gennet.py  (c) Barry Irwin  
 # Generates a series of IP supnets and outputs these as LaTeX or as a CSV
 # In the case of LaTeX - tables are produced both populated and unpopulated
 # Intention is fotr this to be used to generate worksheets for use in classroom
 # enviroments analongside accompanying solutions.
 #
 # USAGE
-#   $ gennet > worksheet.tex; pdflatex worksheet.tex
+#   $ gennet <args> > worksheet.tex; pdflatex worksheet.tex
 #
 # This is intended to be a minimal function and LaTeX formatting can be adjusted in an edditor of choice
+try:
+    import iptools
+except:
+    print("You require the  iptools module to run this script\n pip install iptools")
+    sys.exit(2)
+import random # For random generated IP addresses
+import socket, struct
+import sys, getopt
 
-# Use IPtools package
-import iptools
-
-# For random generated IP addresses
-import random
-import socket
-import struct
 
 ## -------------- some variables to handle data ---------------
-MAXIP=20
-# RFC 3330 special ise cases
-not_valid = [10,127,169,172,192]
-
-#Generates A random IP
-# socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+VERSION = '0.3b'
+CONF={}
+# Default values set
+CONF['maxip']= 20
+CONF['output']= 'L'
+CONF['not_valid']= [10,127,169,172,192] # RFC 3330 special use cases
+CONF['seed']= random.randrange(31337,65535) # this is potentially ~2^32 combinations
+CONF['exline']= True  #include an example line in problem output
+CONF['loutput']= 'full' #by default full output
 
 iplist=[]
+masklist=[]
+cidrlist=[]
 
 # This is used to hold a list of subnets which are used to henerate the following:
 #    [ip/netmask] - IP address/netmask in CIDR
@@ -37,26 +43,18 @@ iplist=[]
 #    [netmask] - in dotted quad
 #    [count] - number of IPs in subnet - not considering availability (2^n-2)
 
-# this is potentially ~2^32 combinations - and can be changed if needed
-seed=random.randrange(31337,65535)
-print(f"% {seed} {hex(seed)}")
 #set the seed explicitly for repeatability
-random.seed(seed)
 
 
 def dqnetmask(prefix):
     return socket.inet_ntoa(struct.pack(">I", (0xffffffff << (32 - prefix)) & 0xffffffff))
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def printlatex(mylist):
-
-
-
-
-def latexpreamble()    
     print ("% Do not edit unless you really know what you are doing")
     print ("\\documentclass[english]{article}")
-#    print ("\\usepackage[T1]{fontenc}")
-#    print ("\\usepackage[latin9]{inputenc}")
     print ("\\usepackage[a4paper]{geometry}")
     print ("\\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.0cm,rmargin=2.0cm,headheight=2.5cm,headsep=2.5cm,footskip=2.5cm}")
     print ("\\usepackage{setspace}")
@@ -65,9 +63,9 @@ def latexpreamble()
     print ("\\providecommand{\\tln}{\\\\}")
     print ("\\makeatother")
     print ("\\newcommand{\\rndseed}{",end='')
-    print (f"{hex(seed)}",end='')
+    print (f"{hex(CONF['seed'])}",end='')
     print ("}",end='')
-    print (f" % {seed}")
+    print (f" % {CONF['seed']}")
     print ("\\begin{document}")
     print ("\\title{IP Subnetting worksheet - \\rndseed}\n\\date{}\n\\maketitle")
     print ("\\pagenumbering{gobble}")  #remove page numbers for improved inclusion elsewhere
@@ -80,25 +78,26 @@ def latexpreamble()
 
     print ("\\end{document}")
 
-def latexproblem(mylist)    
-    print (f"% {hex(seed)} - to be used for later generation of solutions") 
+def latexproblem(mylist):    
+    print (f"% {hex(CONF['seed'])} - to be used for later generation of solutions") 
 
     print ("\\begin{tabular}{|c|c|c|c|c|c|} \hline CIDR &")
     print ("Network & Bcast & IP Count & First IP & DQ Netmask \\tln")
     print ("\\hline\n\\hline")
-    print ("102.204.166.91/16 & 102.204.0.0 & 102.204.255.255 & 65536 & 102.204.0.1 & 255.255.0.0 \\tln")
-    print ("\\hline")
+    if (CONF['exline']):
+        print ("102.204.166.91/16 & 102.204.0.0 & 102.204.255.255 & 65536 & 102.204.0.1 & 255.255.0.0 \\tln")
+        print ("\\hline")
+
     for x in  mylist:
         print (f"{x} &            &            &            &            &            \\tln")
         print ("\\hline")
 
     print ("\\end{tabular}")
 
-def latexsoln(mylist)
+def latexsoln(mylist):
     print ("\\begin{tabular}{|c|c|c|c|c|c|} \hline CIDR &")
     print ("Network & Bcast & IP Count & First IP & DQ Netmask \\tln")
     print ("\\hline\n\\hline")
-
 
     for x in  mylist:
         netname=iptools.ipv4.cidr2block(x)[0]
@@ -112,7 +111,7 @@ def latexsoln(mylist)
     print ("\\end{tabular}\n")
 
 def printcsv(mylist):
-    print(mylist)
+    print ("#IP/mask,IP Count,Netblock,Broadcast,First IP,DQ Netmask")
     for c in mylist:
         netname=iptools.ipv4.cidr2block(c)[0]
         bcast=iptools.ipv4.cidr2block(c)[1]
@@ -122,24 +121,110 @@ def printcsv(mylist):
         dqmask=dqnetmask(int(c[-2:]))
         print (f"{c},{ipcount},{netname},{bcast},{first},{dqmask}")
 
+def usage(name):
+    print("This produced a list of IPv4 subnets to stdout in a variety of formats\n\n")
+    print (f" {name} [-hV] [-px] [-m <num>] [-s <num] [-L [-o val] | -C] ")
+    print ("  -h     This usage guide")
+    print ("  -m <n> Number of IP subnets to generate  (default=20)")
+   # print ("  -4     IPv4 mode (default)")
+    #print ("  -6     IPv6 mode (max subnet of /64)")
+    print ('''  -o <type> Output type for partial output in LaTex mode
+           soln - Solutions table
+           prob - Problems table''')
+    print ("  -p     Include RFC1918/RFC 3300 special use IP addresses (default=no)")
+    print ("  -x     Do not print the example row in the problems table")
+    print ("  -L     Latex Documnet output mode")
+    print ("  -C     CSV output mode")
+    print ("  -S <n> Seed (int or hex - [0x????] )")
+    print ("  -V    Version and source information")
+
+    print(f"\nMinimum examples:\n\t{name} -L # prints a ful LaTeX dump\n\t{name} -C # prints CSV output")
+
+
+def dumpconf():
+    eprint("Dumping Config")
+    eprint(f"{CONF}")
+
+def main(argv):
+    global CONF
+    if(len(sys.argv)<2):
+        usage(sys.argv[0])
+        sys.exit(2)
+    try:
+        opts, args = getopt.getopt(argv[1:],"46hm:o:pxCLS:V",["help","max-ip=","output","version"])
+    except getopt.GetoptError:
+        usage(sys.argv[0])  #thrown if any args dont match or no args given
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h","--help"):
+            usage(sys.argv[0])
+            sys.exit()
+        elif opt in ['-o','--output']:
+            CONF['loutput']=arg
+        elif opt == '-p':
+            CONF['not_valid']=[]
+        elif opt == '-x':
+            CONF['exline'] = False
+        elif opt == '-S':
+            if arg[:2] == '0x' or arg[:2]=='0X':
+                CONF['seed'] = int(arg,base=16)
+            else:
+                CONF['seed'] = int(arg)
+        elif opt in ("-m", "--max-ip"):
+           CONF['maxip'] = int(arg)
+        elif opt in ("-L","-C"):
+           CONF['output'] = opt[1]
+        elif opt in ["-V","--version"]:
+            print(f"Version: {VERSION} (c) 2022 Barry Irwin")
+            print("Available at: https://github.com/barryirwin/gennet")
+            usage(sys.argv[0])
+            sys.exit()
+        elif opt in ["-4","-6"]:
+            print(f"This function \"{opt}\" does not yet know what to do")
+            usage(sys.argv[0])
+            sys.exit()
+
+    random.seed(CONF['seed'])
+   # dumpconf()
 # Real work starts here
-# print out a a list limited by MAXIP
-for i in range(MAXIP):
-    first = random.randrange(1,256)
-    while (first in not_valid) & (first < 224) :  # no multicast space and no special blocks
-        first = random.randrange(1,256)
-
-    ip = ".".join([str(first),str(random.randrange(1,256)),str(random.randrange(1,256)),str(random.randrange(1,256))])
-    iplist.append(ip)
+# print out a a list limited by CONF['maxip']
+    for i in range(int(CONF['maxip'])):
+        first = random.randrange(1,223)
 
 
-for i in iplist:
-    mask=random.randrange(16,29)
-    masklist.append(mask)
-    cidrlist.append("%s/%d" % (i,mask))
+        while ((first in CONF['not_valid']) or (first > 224)) :  # no multicast space and no special blocks
+            first = random.randrange(1,256)
 
+        ip = ".".join([str(first),str(random.randrange(1,256)),str(random.randrange(1,256)),str(random.randrange(1,256))])
+        iplist.append(ip)
+
+    for i in iplist:
+        mask=random.randrange(16,29)
+        masklist.append(mask)
+        cidrlist.append("%s/%d" % (i,mask))
+# now produce outputs based on  instructions
+    otype=CONF['output']
+    loutput=CONF['loutput']
+    if otype == 'C': #CSV dump
+        printcsv(cidrlist)
+        sys.exit(1)
+    elif otype == 'L': # latex options
+        if CONF['loutput']=='prob':
+            latexproblem(cidrlist)
+        elif CONF['loutput']=='soln':
+            latexsoln(cidrlist)
+        else:
+            printlatex(cidrlist)
 #print(cidrlist)
 #printcsv(cidrlist)
-printlatex(cidrlist)
+#printlatex(cidrlist)
 
+if __name__ == "__main__":
+   main(sys.argv)
+
+
+# -- fin --
+#Generates A random IP
+# socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
 
